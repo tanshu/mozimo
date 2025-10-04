67411b3972a3f0ca150f6da77574d4895cb28e7c
Amritanshu <git@tanshu.com>
2025-10-04 11:31:00 +0000

    Update build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
+  // optional hardening
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
